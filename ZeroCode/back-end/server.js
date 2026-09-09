e5fe97a953021b0e5f2c34aacc2f9b84b3b7cf19
Basudeb Bej <basudebbej2025@gmail.com>
2026-09-09 21:18:01 +0530

    Update server.js

--- ZeroCode/back-end/server.js
+++ ZeroCode/back-end/server.js
@@ -1,4 +1,6 @@
 // back-end/server.js
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
